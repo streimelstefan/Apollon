01252ee944f6d82d24a08d4ab16c14fea6559290
Stefan Streimel <stefan@streimel.com>
2024-05-19 12:09:07 +0200

    Moved ExactUnifier to the Unifiers

--- Template.Lib/Unification/ExactUnifier.cs
+++ Template.Lib/Unification/ExactUnifier.cs
@@ -1,6 +1,5 @@
 ﻿using Apollon.Lib.Graph;
 using Apollon.Lib.Rules;
-using Apollon.Lib.Unification;
 using Apollon.Lib.Unification.Substitutioners;
 using System;
 using System.Collections.Generic;
@@ -8,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Apollon.Lib.Resolution.Checkers
+namespace Apollon.Lib.Unification
 {
     public class ExactUnifier : IUnifier
     {
