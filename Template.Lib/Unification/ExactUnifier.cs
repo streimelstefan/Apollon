9c151699c0be62a181e77167af3da612d7852237
Stefan Streimel <stefan@streimel.com>
2024-05-19 14:05:56 +0200

    Changed Exact Unifier so it counts two variables as the same

--- Template.Lib/Unification/ExactUnifier.cs
+++ Template.Lib/Unification/ExactUnifier.cs
@@ -1,9 +1,12 @@
-﻿using Apollon.Lib.Graph;
+﻿using Apollon.Lib.Atoms;
+using Apollon.Lib.Graph;
 using Apollon.Lib.Rules;
+using Apollon.Lib.Rules.Operations;
 using Apollon.Lib.Unification.Substitutioners;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,38 +18,117 @@ namespace Apollon.Lib.Unification
 
         public UnificationResult Unify(Literal unifier, Literal against, ISubstitution sigma)
         {
-            if (unifier.Equals(against))
-            {
-                return new UnificationResult(new Substitution());
-            }
-            return new UnificationResult("Are not exact match");
+
+            return Unify(unifier, against);
         }
 
         public UnificationResult Unify(BodyPart unifier, BodyPart against, ISubstitution sigma)
         {
-            if (unifier.Equals(against))
-            {
-                return new UnificationResult(new Substitution());
-            }
-            return new UnificationResult("Are not exact match");
+            return Unify(unifier, against);
         }
 
         public UnificationResult Unify(Literal unifier, Literal against)
         {
-            if (unifier.Equals(against))
+            var res = Unify(unifier.Atom, against.Atom);
+            if (res.IsError)
             {
-                return new UnificationResult(new Substitution());
+                return res;
             }
-            return new UnificationResult("Are not exact match");
+
+            if (unifier.IsNAF != against.IsNAF)
+            {
+                return new UnificationResult($"NAF is not the same | {unifier} \\= {against}");
+            }
+            if (unifier.IsNegative != against.IsNegative)
+            {
+                return new UnificationResult($"Negation is not the same | {unifier} \\= {against}");
+            }
+
+            return res;
         }
 
         public UnificationResult Unify(BodyPart unifier, BodyPart against)
         {
-            if (unifier.Equals(against))
+            if (unifier.IsLiteral != against.IsLiteral || unifier.IsForAll != against.IsForAll || 
+                unifier.HasChild != against.HasChild || unifier.IsOperation != unifier.IsOperation)
             {
+                return new UnificationResult($"Type does not match | {unifier} \\= {against}");
+            }
+
+            // here for all can be ignored since the forall term needs to be varaible anyway
+
+            if (unifier.Literal != null && against.Literal != null)
+            {
+                return Unify(unifier.Literal, against.Literal);
+            }
+
+            if (unifier.Operation != null && against.Operation != null)
+            {
+                return Unify(unifier.Operation, against.Operation);
+            }
+
+            if (unifier.Child != null && against.Child != null)
+            {
+                return Unify(unifier.Child, against.Child);
+            }
+
+            throw new InvalidOperationException($"Unahndled Case within Exact Unification => {unifier} {against}");
+        }
+
+        public UnificationResult Unify(Atom atom1, Atom atom2)
+        {
+            if (atom1.Name != atom2.Name)
+                return new UnificationResult($"Name missmatch => {atom1} \\= {atom2}");
+
+            if (atom1.ParamList.Length != atom2.ParamList.Length)
+                return new UnificationResult($"ParamList length missmatch => {atom1} \\= {atom2}");
+
+            for (int i = 0; i < atom1.ParamList.Length; i++)
+            {
+                var res = Unify(atom1.ParamList[i], atom2.ParamList[i]);
+                if (res.IsError)
+                {
+                    return res;
+                }
+            }
+
+            return new UnificationResult(new Substitution());
+        }
+
+        public UnificationResult Unify(AtomParam param1, AtomParam param2)
+        {
+            if (param1.IsTerm != param2.IsTerm || param2.IsLiteral != param1.IsLiteral)
+                return new UnificationResult($"Type missmatch => {param1} \\= {param2}");
+
+            if (param1.Literal != null && param2.Literal != null)
+                return Unify(param1.Literal, param2.Literal);
+            
+            if (param1.Term != null && param2.Term != null)
+            {
+                if (param2.Term.IsVariable != param1.Term.IsVariable)
+                    return new UnificationResult($"Type missmatch => {param1} \\= {param2}");
+                if (param1.Term.IsVariable && param2.Term.IsVariable)
+                    return new UnificationResult(new Substitution());
+
+                if (param1.Term.Value !=  param2.Term.Value)
+                    return new UnificationResult($"Name missmatch => {param1} \\= {param2}");
+
                 return new UnificationResult(new Substitution());
             }
-            return new UnificationResult("Are not exact match");
+
+            throw new InvalidOperationException($"Unhandled Case in Exact Unification => {param1} {param2}");
+        }
+
+        public UnificationResult Unify(Operation op1, Operation op2)
+        {
+            var res = Unify(op1.Variable, op2.Variable);
+            if (res.IsError)
+                return res;
+
+            if (op1.Operator != op2.Operator)
+                return new UnificationResult($"Operation missmatch => {op1} \\= {op2}");
+
+            return Unify(op1.Condition, op2.Condition);
         }
     }
 }
---
+++ Template.Test/ExactUnifierTests.cs
@@ -0,0 +1,84 @@
+﻿using Apollon.Lib;
+using Apollon.Lib.Atoms;
+using Apollon.Lib.Rules;
+using Apollon.Lib.Rules.Operations;
+using Apollon.Lib.Unification;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apollon.Test
+{
+    [TestFixture]
+    public class ExactUnifierTests
+    {
+
+        private IUnifier _unifier = new ExactUnifier();
+
+        [SetUp]
+        public void Setup()
+        {
+            _unifier = new ExactUnifier();
+        }
+
+        [Test]
+        public void ShouldUnifyIfTheyAreExactlyTheSame()
+        {
+            var lit1 = new Literal(new Atom("a", new AtomParam(new Term("X"))), false, false);
+            var lit2 = new Literal(new Atom("a", new AtomParam(new Term("X"))), false, false);
+            
+            var res = _unifier.Unify(lit1, lit2);
+
+            Assert.IsTrue(res.IsSuccess);
+            Assert.AreEqual(0, res.Value.Mappings.Count());
+        }
+
+        [Test]
+        public void ShouldUnifyIfOnlyVariableNamesAreDifferent()
+        {
+            var lit1 = new Literal(new Atom("a", new AtomParam(new Term("X"))), false, false);
+            var lit2 = new Literal(new Atom("a", new AtomParam(new Term("Y"))), false, false);
+
+            var res = _unifier.Unify(lit1, lit2);
+
+            Assert.IsTrue(res.IsSuccess);
+            Assert.AreEqual(0, res.Value.Mappings.Count());
+        }
+
+        [Test]
+        public void ShouldNotUnifyIfAnythingButVariableNamesAreDifferent()
+        {
+            var lit1 = new Literal(new Atom("a", new AtomParam(new Term("X"))), false, false);
+            var lit2 = new Literal(new Atom("b", new AtomParam(new Term("X"))), false, false);
+
+            var res = _unifier.Unify(lit1, lit2);
+
+            Assert.IsFalse(res.IsSuccess);
+        }
+
+        [Test]
+        public void ShouldUnifyOperationWithVariableNameDifferences()
+        {
+            var op1 = new Operation(new AtomParam(new Term("X")), Operator.Equals, new Literal(new Atom("a"), false, false));
+            var op2 = new Operation(new AtomParam(new Term("Y")), Operator.Equals, new Literal(new Atom("a"), false, false));
+
+            var res = _unifier.Unify(new BodyPart(null, op1), new BodyPart(null, op2));
+
+            Assert.IsTrue(res.IsSuccess);
+        }
+
+        [Test]
+        public void ShouldUnifyForAllWithMissmatchedVariableNames()
+        {
+            var bp1 = new BodyPart(new Term("X"), new Literal(new Atom("a"), false, false));
+            var bp2 = new BodyPart(new Term("Y"), new Literal(new Atom("a"), false, false));
+
+            var res = _unifier.Unify(bp1, bp2);
+
+            Assert.IsTrue(res.IsSuccess);
+        }
+    }
+}
